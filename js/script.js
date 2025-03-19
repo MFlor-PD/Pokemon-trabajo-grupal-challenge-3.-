8b2cf689a0ba268a6700e7fe31ce046f217df160
osama <obg1309@gmail.com>
2025-03-19 13:37:19 +0100

    Version beta

--- js/script.js
+++ js/script.js
@@ -1,30 +1,27 @@
 
-
 const btnGetPokemon = document.getElementById("get-pokemon"); //Boton
 const infoPokemonDIV = document.getElementById("info-pokemon"); //Contenedor
 let selectedPokemon;
 let pokemon_types;
 
-/*function getTypesPokemon (types){
+function getTypesPokemon (){
     let text ="";
-    types.forEach(element => {
-        //console.log(element.type.url)
+    pokemon_types.forEach(element => {
+        //console.log(element)
         fetch(`${element.type.url}`)
         .then((response) => {
             if (!response.ok) throw new Error('Error en la solicitud');
             return response.json();
         })
         .then((data) => {
-            console.log(data);
-            console.log(data.sprites["generation-iii"].colosseum.name_icon);
+            //console.log(data);
+            //console.log(data.sprites["generation-iii"].colosseum.name_icon);
         
-            text += `<img src= ${data.sprites["generation-iii"].colosseum.name_icon}>`;
+            text = `<img src= ${data.sprites["generation-iii"].colosseum.name_icon}>`;
             infoPokemonDIV.innerHTML +=text;
         })
     });
 }
-*/
-
 
 function getPokemon (){
     fetch(`https://pokeapi.co/api/v2/pokemon/${selectedPokemon}`)
@@ -33,20 +30,19 @@ function getPokemon (){
         return response.json();
     })
     .then((data) => {
-        console.log(data);
+        //console.log(data);
         //getTypesPokemon(data.types);
         pokemon_types = data.types;
-        const pokemonTypes = data.types.map((type) => type.type.name).join(', ');
+        //const pokemonTypes = data.types.map((type) => type.type.name).join(', ');
         infoPokemonDIV.innerHTML =  
             `<div class="character">
                     <img src=${data.sprites.front_default}>
                     <h3>${data.name}</h3>
-                    <p>${pokemonTypes}</p>
-                    <p>${data.weight}</p>
-                    <p>${data.height}</p>
+                    <p>Peso: ${data.weight}</p>
+                    <p>Altura: ${data.height}</p>
                 </div>`;
-        
-        
+    }).then(() => {
+        getTypesPokemon();
     })
     .catch((error) => {
         console.error("Error: No se pudo procesar la solicitud")
@@ -54,11 +50,8 @@ function getPokemon (){
 
 }
 
-
 btnGetPokemon.addEventListener("click", () => {
     selectedPokemon = document.getElementById("pokemon-select").value; // Pokemon Seleccionado
-    getPokemon();
-    
-    
-
+    infoPokemonDIV.style="";
+    getPokemon()
 })
